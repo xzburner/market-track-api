58b5136360e93c31f1971ecc9aed3577fc292b02
Junio Souza <ejunioms@gmail.com>
2024-07-23 15:40:56 -0300

    create: Creating basic strutucture

--- src/index.js
+++ src/index.js
@@ -1,22 +1,45 @@
 const express = require('express');
 const axios = require('axios');
-
 const app = express();
+const port = 3000;
 
-app.get('/yahoo-finance', async (req, res) => {
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.get('/historical-data', async (req, res) => {
+  const { symbol, startDate, endDate } = req.query;
+
+  if (!symbol || !startDate || !endDate) {
+    return res.status(400).json({ error: 'Missing required parameters' });
+  }
+
   try {
-    const { symbol } = req.query; // Obtém o símbolo da ação dos parâmetros da requisição
-    const response = await axios.get(`https://api.example.com/finance?symbol=${symbol}`, {
-      headers: {
-        'x-api-key': 'SUA_API_KEY' // Inclua a chave da API se necessário
-      }
+    // Convertendo datas para timestamps Unix
+    const period1 = Math.floor(new Date(startDate).getTime() / 1000);
+    const period2 = Math.floor(new Date(endDate).getTime() / 1000);
+
+    const response = await axios.get('https://query1.finance.yahoo.com/v8/finance/chart/' + symbol, {
+      params: {
+        period1,
+        period2,
+        interval: '1d',
+        events: 'history'
+      },
+      responseType: 'text'
     });
-    res.json(response.data); // Envia os dados da API para o cliente
+
+    const result = JSON.parse(response.data);
+
+    res.header('Content-Type', 'text/json');
+    res.send(result);
   } catch (error) {
-    res.status(500).send(error.toString()); // Lida com erros e envia uma resposta apropriada
+    console.error('Error fetching data', error);
+    res.status(500).json({ error: 'Failed to fetch data from Yahoo Finance' });
   }
 });
 
-app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
+app.listen(port, () => {
+  console.log(`Server running at port:${port}`);
 });
